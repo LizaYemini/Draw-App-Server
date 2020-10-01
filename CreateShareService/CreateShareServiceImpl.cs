aa7e9b43b988bf093e5b6d29b059174baca58b78
LizaYemini <51076530+LizaYemini@users.noreply.github.com>
2020-10-01 17:28:55 +0300

    Add Create Share Service

---
+++ CreateShareService/CreateShareServiceImpl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Contracts;
+using DrawPicContracts.DTO;
+using DrawPicContracts.Interface;
+using InfraContracts.DTO;
+
+namespace CreateShareService
+{
+    [Register(Policy.Transient, typeof(ICreateShareService))]
+    public class CreateShareServiceImpl : ICreateShareService
+    {
+        private readonly IShareDal _dal;
+
+        public CreateShareServiceImpl(IShareDal dal)
+        {
+            _dal = dal;
+        }
+        public Response CreateShare(CreateShareRequest request)
+        {
+            try
+            {
+                _dal.CreateShare(request);
+                CreateShareResponseOk ret = new CreateShareResponseOk();
+                return ret;
+            }
+            catch (Exception ex)
+            {
+                return new AppResponseError(ex.Message);
+            }
+        }
+    }
+}
